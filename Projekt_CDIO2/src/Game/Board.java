dc9171d013ab729f591748259ad1a010d170a094
AoschkA <jonaspraem@gmail.com>
2014-10-31 16:55:57 +0100

    Rettet Array i Board

--- Projekt_CDIO2/src/Game/Board.java
+++ Projekt_CDIO2/src/Game/Board.java
@@ -12,22 +12,10 @@ public class Board {
 	Tile tile9;
 	Tile tile10;
 	Tile tile11;	
-	Tile board[];
+	Tile board[] = {tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9, tile10, tile11};
+	
 	
 	public Board(Tile tile1, Tile tile2, Tile tile3, Tile tile4, Tile tile5, Tile tile6, Tile tile7, Tile tile8, Tile tile9, Tile tile10, Tile tile11) {
-		Tile board[] = new Tile[11];
-		board[0] = this.tile1;
-		board[1] = this.tile2;
-		board[2] = this.tile3;
-		board[3] = this.tile4;
-		board[4] = this.tile5;
-		board[5] = this.tile6;
-		board[6] = this.tile7;
-		board[7] = this.tile8;
-		board[8] = this.tile9;
-		board[9] = this.tile10;
-		board[10] = this.tile11;
-		
 		this.tile1 = tile1;
 		this.tile2 = tile2;
 		this.tile3 = tile3;
@@ -69,7 +57,7 @@ public class Board {
 		String n = "0";
 		for (int i=0; i<=10; i++) {
 		n = (board[i].toString() + "\n");
-		}
+	}
 		return n;
 	}
 }
