5f0333795107b989a875eeaf32b4c3df99acd13f
AoschkA <jonaspraem@gmail.com>
2014-10-31 13:08:31 +0100

    Ændret Tileantal i board fra 12 til 11

--- Projekt_CDIO2/src/Game/Board.java
+++ Projekt_CDIO2/src/Game/Board.java
@@ -11,12 +11,11 @@ public class Board {
 	Tile tile8;
 	Tile tile9;
 	Tile tile10;
-	Tile tile11;
-	Tile tile12;	
+	Tile tile11;	
 	Tile board[];
 	
-	public Board(Tile tile1, Tile tile2, Tile tile3, Tile tile4, Tile tile5, Tile tile6, Tile tile7, Tile tile8, Tile tile9, Tile tile10, Tile tile11, Tile tile12) {
-		Tile board[] = new Tile[12];
+	public Board(Tile tile1, Tile tile2, Tile tile3, Tile tile4, Tile tile5, Tile tile6, Tile tile7, Tile tile8, Tile tile9, Tile tile10, Tile tile11) {
+		Tile board[] = new Tile[11];
 		board[0] = this.tile1;
 		board[1] = this.tile2;
 		board[2] = this.tile3;
@@ -28,7 +27,6 @@ public class Board {
 		board[8] = this.tile9;
 		board[9] = this.tile10;
 		board[10] = this.tile11;
-		board[11] = this.tile12;
 		
 		this.tile1 = tile1;
 		this.tile2 = tile2;
@@ -41,7 +39,6 @@ public class Board {
 		this.tile9 = tile9;
 		this.tile10 = tile10;
 		this.tile11 = tile11;
-		this.tile12 = tile12;
 	}
 	
 	public String getTilemessage(int tilenumber) {
@@ -70,7 +67,7 @@ public class Board {
 	
 	public String toString() {
 		String n = "0";
-		for (int i=0; i<=11; i++) {
+		for (int i=0; i<=10; i++) {
 		n = (board[i].toString() + "\n");
 		}
 		return n;
